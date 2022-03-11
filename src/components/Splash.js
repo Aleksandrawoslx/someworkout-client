96cf17f4305d9b37fd0f39f6ebab5bb3908651a6
Aleksandra Wos <aleksandra1.wos@gmail.com>
2022-03-11 12:13:24 +0000

    pop-up greet

--- src/components/Splash.js
+++ src/components/Splash.js
@@ -1,9 +1,28 @@
 import {Box, Heading, Image} from "grommet"
 import splashImg from "../splash.jpg"
+import { Notification } from "grommet"
+import {useContext, useState} from "react"
+import { AuthContext } from "../context/auth.context"
 
 function Splash() {
+
+  const {user} = useContext(AuthContext);
+  const [visibleLogin, setVisibleLogin] =useState(true)
+
+  const greetName = user ? user.name : "!"
+
+
   return (
     <Box>
+      { (visibleLogin && user) && <Notification
+        container={{ background: { color: "background-front" } }}
+        toast={{ position: "top-right" }}
+        background="brand"
+        status="normal"
+        title="You have logged in"
+        message={`Hello ${greetName}`}
+        onClose={() => setVisibleLogin(false)}
+      />}
         <Heading>
             SomeWorkout
         </Heading>
